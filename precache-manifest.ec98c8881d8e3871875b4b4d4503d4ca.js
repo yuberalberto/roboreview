1cdc4108f2d4170b886a3c7e2b7e8b449f99217e
yuberalberto <yuberalberto@gmail.com>
2019-01-31 15:41:05 -0500

    Updates

---
+++ precache-manifest.ec98c8881d8e3871875b4b4d4503d4ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "148623bffcb8eeeaf161",
+    "url": "/roboreview/static/js/runtime~main.148623bf.js"
+  },
+  {
+    "revision": "5102fc52784ce9c607f2",
+    "url": "/roboreview/static/js/main.5102fc52.chunk.js"
+  },
+  {
+    "revision": "81d24b5af5e5a185a242",
+    "url": "/roboreview/static/js/1.81d24b5a.chunk.js"
+  },
+  {
+    "revision": "5102fc52784ce9c607f2",
+    "url": "/roboreview/static/css/main.dc5246ab.chunk.css"
+  },
+  {
+    "revision": "81d24b5af5e5a185a242",
+    "url": "/roboreview/static/css/1.dbcd085c.chunk.css"
+  },
+  {
+    "revision": "474cfa74ec0ea4c1bcb02da9415cd20c",
+    "url": "/roboreview/index.html"
+  }
+];
